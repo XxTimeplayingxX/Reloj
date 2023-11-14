b237cd7c559336debec79e62de4761c29d8365cc
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2023-11-13 21:28:13 -0500

    Agregar archivos de proyecto.

---
+++ Reloj/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Reloj
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Reloj/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Reloj
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
